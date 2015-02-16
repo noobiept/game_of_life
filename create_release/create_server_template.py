9a2bb4e869a2886d2625bcd344751ac0e5aa32a4
Pedro Ferreira <plferreira4@gmail.com>
2015-02-16 18:49:40 +0000

    - fix create_server_template script

--- create_release/create_server_template.py
+++ create_release/create_server_template.py
@@ -1,28 +1,20 @@
 # python3
 
 import os.path
+import argparse
 import sys
 import json
 
 PROJECTS_PATH = '../../'
+DIR_PATH = os.path.dirname( os.path.realpath( __file__ ) )
+
 
 sys.path.append( PROJECTS_PATH )
 
 from create_release_script import create_server_template
 
 
-def go():
-
-    indexPath = '../index.html'
-    manifestPath = '../manifest.json'
-
-    with open( manifestPath, 'r', encoding= 'utf-8' ) as f:
-        manifest = f.read()
-
-    manifest = json.loads( manifest )
-    name = manifest[ 'name' ]
-
-    destinationPath = os.path.join( PROJECTS_PATH, 'website/templates/{}/'.format( name ) )
+def go( indexPath, appName, destinationPath ):
 
     indexPath = os.path.realpath( indexPath )
     copyToPath = os.path.realpath( destinationPath )
@@ -30,6 +22,25 @@ def go():
     create_server_template.go( indexPath, name, copyToPath )
 
 
+
 if __name__ == '__main__':
 
-    go()
+    manifestPath = os.path.join( DIR_PATH, '../manifest.json' )
+
+    with open( manifestPath, 'r', encoding= 'utf-8' ) as f:
+        manifest = f.read()
+
+    manifest = json.loads( manifest )
+    name = manifest[ 'name' ]
+    defaultIndex = '../index.html'
+    defaultDestination = os.path.join( PROJECTS_PATH, 'website/templates/{}'.format( name ) )
+
+    parser = argparse.ArgumentParser( description= 'Create the server template.' )
+
+    parser.add_argument( 'indexPath', nargs= '?', default= defaultIndex )
+    parser.add_argument( 'appName', nargs= '?', default= name )
+    parser.add_argument( 'copyToPath', nargs= '?', default= defaultDestination )
+
+    args = parser.parse_args()
+
+    go( args.indexPath, args.appName, args.copyToPath )
