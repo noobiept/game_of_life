96ef8de5d609ce6784a3bcfdea9cf56a46b5a957
Pedro Ferreira <plferreira4@gmail.com>
2023-10-27 23:26:56 +0200

    ✨: Add github actions.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
+import { CanvasComponent } from './canvas/canvas.component'
+import { MenuComponent } from './menu/menu.component'
 
 describe('AppComponent', () => {
     beforeEach(() =>
         TestBed.configureTestingModule({
             imports: [RouterTestingModule],
-            declarations: [AppComponent],
+            declarations: [AppComponent, CanvasComponent, MenuComponent],
         })
     )
 
@@ -22,12 +24,10 @@ describe('AppComponent', () => {
         expect(app.title).toEqual('game_of_life')
     })
 
-    it('should render title', () => {
+    it('should have a canvas', () => {
         const fixture = TestBed.createComponent(AppComponent)
         fixture.detectChanges()
         const compiled = fixture.nativeElement as HTMLElement
-        expect(compiled.querySelector('.content span')?.textContent).toContain(
-            'game_of_life app is running!'
-        )
+        expect(compiled.querySelector('canvas')).toBeTruthy()
     })
 })
